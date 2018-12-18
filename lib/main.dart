7afbc695d7f0472c38873f3934b0b69ea85fb524
aaus98 <adrianubillus12@gmail.com>
2018-12-18 04:00:26 -0500

    se acaba demo

--- lib/main.dart
+++ lib/main.dart
@@ -41,16 +41,63 @@ class _HomePageState extends State<HomePage> {
   }
 
   void navigationTapped(int page) {
-    // Animating to the page.
-    // You can use whatever duration and curve you like
     _pageController.animateToPage(page,
-        duration: const Duration(milliseconds: 300), curve: Curves.ease);
+        duration: const Duration(milliseconds: 350), curve: Curves.easeInOut);
   }
 
   void onPageChanged(int page) {
     setState(() {
       this._page = page;
     });
+  }
+
+  void _showModalSheet(int index) {
+    print(index);
+    showModalBottomSheet(
+      context: context,
+      builder: (builder) {
+        return Container(
+          padding: EdgeInsets.all(15.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(papers[index]['image']),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                width: 200,
+                height: 200,
+              ),
+              Padding(padding: EdgeInsets.only(top: 12)),
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                      child: 
+                        Text(
+                          papers[index]['name'],
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20
+                          ),
+                        ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 10),),
+                    Center(
+                      child: Text(papers[index]['speaker'],textAlign: TextAlign.center,),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
   }
 
   var papers = [
@@ -64,6 +111,14 @@ class _HomePageState extends State<HomePage> {
     {'name':'Innovación y Nuevas Tecnologías','speaker':'Ing. Juan José Flores Cueto','image':'https://scontent.flim5-3.fna.fbcdn.net/v/t1.0-9/46505939_2184556485195497_286744235862917120_n.png?_nc_cat=105&_nc_ht=scontent.flim5-3.fna&oh=6358ef135b4efbc72c85f6d01efbbef4&oe=5C9FAA27'},
   ];
 
+  var events = [
+    {"name":"Bingo Navideño","date":"Miercoles 19 de diciembre","image":"https://www.castlecourthotel.ie/upload/slide_images/bingo3.jpg"},
+    {"name":"Campeonato INFOFEST - Voley y Futbol","date":"Jueves 20 de diciembre","image":"https://scontent.flim10-1.fna.fbcdn.net/v/t1.0-9/48051632_2194221644228981_1018620467126206464_n.jpg?_nc_cat=110&_nc_ht=scontent.flim10-1.fna&oh=9f94ccc2ec59334ee9400975531e43a0&oe=5CAB4690"},
+    {"name":"Concurso de programación","date":"Viernes 21 de diciembre","image":"https://scontent.flim5-1.fna.fbcdn.net/v/t1.0-9/47137336_2189867807997698_1829595526236995584_n.png?_nc_cat=110&_nc_ht=scontent.flim5-1.fna&oh=53935c459bc833e853f33e0e9c704162&oe=5CA5D4C0"},
+    {"name":"Torneo de videojuegos","date":"Viernes 21 de diciembre","image":"https://i.blogs.es/de368e/dota-2-wallpaper/450_1000.jpg"},
+    {"name":"Fiesta de confraternidad","date":"Viernes 21 de diciembre","image":"https://scontent.flim5-1.fna.fbcdn.net/v/t1.0-9/47687958_2198403750477437_2686912397708361728_n.jpg?_nc_cat=104&_nc_ht=scontent.flim5-1.fna&oh=f9e24c8e45033157b2d8ddd2203d1ba2&oe=5C8B7942"},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,7 +129,8 @@ class _HomePageState extends State<HomePage> {
       ),
       body: PageView(
         children: [
-          Papers(papers: papers),
+          Papers(papers: papers,showModalSheet:_showModalSheet),
+          Events(events: events),
         ],
         onPageChanged: onPageChanged,
         controller: _pageController,
@@ -87,10 +143,10 @@ class _HomePageState extends State<HomePage> {
           items: [
             BottomNavigationBarItem(
               icon: Icon(
-                Icons.home,
+                Icons.chat,
               ),
               title: Text(
-                "Inicio",
+                "Ponencias",
               )
             ),
             BottomNavigationBarItem(
@@ -112,43 +168,110 @@ class _HomePageState extends State<HomePage> {
 
 class Papers extends StatelessWidget {
 
-  Papers({Key key, this.papers}) : super(key: key);
+  Papers({Key key, this.papers,this.showModalSheet}) : super(key: key);
   final papers;
+  final showModalSheet;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      padding: EdgeInsets.symmetric(horizontal: 15.0,vertical: 10.0),
       itemCount: papers.length,
       itemBuilder: (BuildContext context,int index){
-        return Card(
-          margin: EdgeInsets.only(top: 10,left: 15,right: 15),
-          child: Row(
-            children: <Widget>[
-              Container(
-                width: 80,
-                height: 80,
-                child: FadeInImage.assetNetwork(
-                  fadeInCurve: Curves.fastOutSlowIn,
-                  placeholder: 'assets/images/image_placeholder.png',
-                  image: papers[index]['image'],
-                  fit: BoxFit.fitWidth,
+        return GestureDetector(
+          onTap: ()=>showModalSheet(index),
+          child: Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 80,
+                  height: 80,
+                  child: FadeInImage.assetNetwork(
+                    fadeInCurve: Curves.fastOutSlowIn,
+                    placeholder: 'assets/images/image_placeholder.png',
+                    image: papers[index]['image'],
+                    fit: BoxFit.fitWidth,
+                  ),
+                  margin: EdgeInsets.only(right: 15),
                 ),
-                margin: EdgeInsets.only(right: 15),
-              ),
-              Flexible(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(papers[index]['name']),
-                    Padding(padding: EdgeInsets.only(top: 10),),
-                    Text(papers[index]['speaker']),
-                  ],
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(papers[index]['name']),
+                      Padding(padding: EdgeInsets.only(top: 10),),
+                      Text(papers[index]['speaker']),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
+    );
+  }
+}
+
+class Events extends StatelessWidget {
+
+  Events({Key key, this.events}) : super(key: key);
+  final events;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.symmetric(horizontal: 15.0,vertical: 10.0),
+      itemCount: events.length,
+      // scrollDirection: Axis.horizontal,
+      itemBuilder: (BuildContext context,int index){
+        return Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0),),
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(15.0),
+              image: DecorationImage(
+                colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.65), BlendMode.srcOver),
+                image: NetworkImage(events[index]['image']),
+                fit: BoxFit.cover,
+              ),
+            ),
+            height: 250,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Flexible(
+                  child: Center(
+                    child: Text(
+                      events[index]['name'],
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  )
+                ),
+                Flexible(
+                  child: Center(
+                    child: Text(
+                      events[index]['date'],
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  )
+                ),
+              ],
+            ),
+          ),
+        );
+      }
     );
   }
 }
